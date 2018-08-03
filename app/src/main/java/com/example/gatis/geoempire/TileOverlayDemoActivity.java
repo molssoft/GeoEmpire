ade962ab303a05de555eca5e93cc244f3d0f6554
gatis <gatiscamans@inbox.lv>
2018-08-03 18:37:21 +0300

    added demo tiles

---
+++ app/src/main/java/com/example/gatis/geoempire/TileOverlayDemoActivity.java
@@ -0,0 +1,96 @@
+package com.example.gatis.geoempire;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
+import com.google.android.gms.maps.model.UrlTileProvider;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Locale;
+
+/**
+ * This demonstrates how to add a tile overlay to a map.
+ */
+public class TileOverlayDemoActivity extends AppCompatActivity
+        implements OnSeekBarChangeListener, OnMapReadyCallback {
+
+    private static final int TRANSPARENCY_MAX = 100;
+
+    /** This returns moon tiles. */
+    private static final String MOON_MAP_URL_FORMAT =
+            "https://www.manfy.lv/wp-content/uploads/2017/05/green.png";
+
+    private TileOverlay mMoonTiles;
+    private SeekBar mTransparencyBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tile_overlay_demo);
+
+        mTransparencyBar = (SeekBar) findViewById(R.id.transparencySeekBar);
+        mTransparencyBar.setMax(TRANSPARENCY_MAX);
+        mTransparencyBar.setProgress(0);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.setMapType(GoogleMap.MAP_TYPE_NONE);
+
+        TileProvider tileProvider = new UrlTileProvider(256, 256) {
+            @Override
+            public synchronized URL getTileUrl(int x, int y, int zoom) {
+                // The moon tile coordinate system is reversed.  This is not normal.
+                int reversedY = (1 << zoom) - y - 1;
+                String s = String.format(Locale.US, MOON_MAP_URL_FORMAT, zoom, x, reversedY);
+                URL url = null;
+                try {
+                    url = new URL(s);
+                } catch (MalformedURLException e) {
+                    throw new AssertionError(e);
+                }
+                return url;
+            }
+        };
+
+        mMoonTiles = map.addTileOverlay(new TileOverlayOptions().tileProvider(tileProvider));
+        mTransparencyBar.setOnSeekBarChangeListener(this);
+    }
+
+    public void setFadeIn(View v) {
+        if (mMoonTiles == null) {
+            return;
+        }
+        mMoonTiles.setFadeIn(((CheckBox) v).isChecked());
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (mMoonTiles != null) {
+            mMoonTiles.setTransparency((float) progress / (float) TRANSPARENCY_MAX);
+        }
+    }
+}
